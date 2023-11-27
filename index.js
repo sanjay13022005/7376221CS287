5483fb260c54ffa312bc2e254babd6ca7080f26e
sanjay13022005 <131228828+sanjay13022005@users.noreply.github.com>
2023-11-27 16:49:16 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-const PORT = 4000
-app.get('/',(req,res) =>
-     res.send('Hello World-7376221CS287-SANJAY G'))
-app.listen(PORT,() =>console.log('Example app listening at http://localhost:${PORT}'))
